2e1dfb0d5ad0b55cde397b3f4d27a89ae0814df3
Marchukwu <nwekeamarachukwu28@gmail.com>
2023-02-19 14:19:12 +0100

    update

--- src/components/database.js
+++ src/components/database.js
@@ -8,10 +8,10 @@ export const Leaderboard = [
   },
   {
     name: "Chukwudera Aniehe",
-    details: "Track : Frontend; stacks: Html, CSS, Reactjs and Native",
+    details: "Track : Backend; stacks: API, CSS, Mongolia and Solidity",
     score: 2310,
     img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2023-02-10",
+    dt: "2023-02-13",
   },
   {
     name: "Dickson Divine",
@@ -32,14 +32,14 @@ export const Leaderboard = [
     details: "Track : Frontend; stacks: Html, CSS, Reactjs and Native",
     score: 3450,
     img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2023-02-10",
+    dt: "2023-02-18",
   },
   {
     name: "Emmanuel Ekete",
     details: "Track : Web3; stacks: Solidity, Mongodb, nodejs and express",
     score: 3250,
     img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2023-02-10",
+    dt: "2023-02-17",
   },
 ];
 
